c7d912fcdc808c5d0acd392b6e9604dce69d76cc
Cute Luma <eeveenly@gmail.com>
2021-10-05 15:33:50 -0300

    Launcher Fixes

---
+++ LauncherFiles/LauncherFiles/Source/Arquivos.cs
@@ -0,0 +1,7 @@
+﻿namespace LauncherFiles.Source{
+    public class Arquivos{
+        public int FileID { get; set; }
+        public string FileHash { get; set; }
+        public string FilePath { get; set; }
+    }
+}
---
+++ LauncherFiles/LauncherFiles/Source/Info.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Crc32C;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace LauncherFiles.Source{
+    public class Info{
+        string FileDir = AppDomain.CurrentDomain.BaseDirectory;
+        public List<Arquivos> FileInfoz = new List<Arquivos>();
+        public static byte[] ConverteStreamToByteArray(Stream stream){
+            byte[] byteArray = new byte[16 * 1024];
+            using(MemoryStream mStream = new MemoryStream()){
+                int bit;
+                while((bit = stream.Read(byteArray, 0, byteArray.Length)) > 0){
+                    mStream.Write(byteArray, 0, bit);
+                }
+                return mStream.ToArray();
+            }
+        }
+        public string GetChecksumBuffered(string filePath){
+            var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            MemoryStream ms = new MemoryStream((int)stream.Length);
+            stream.CopyTo(ms);
+            uint crc = Crc32CAlgorithm.Compute(ms.ToArray());
+            return crc.ToString();
+        }
+        public void FileListCreate(){
+            int FileID = 1;
+            string[] allfiles = Directory.GetFiles(FileDir, "*.*", SearchOption.AllDirectories);
+            foreach(var file in allfiles){
+                FileInfo info = new FileInfo(file);
+                if(info.Name != "LauncherFiles.exe" && info.Name != "grandchase.dll" && info.Name != "grandchase.exe"){
+                    string hashFinal = null;
+                    hashFinal = GetChecksumBuffered(@info.DirectoryName + "\\" + info.Name);
+                    Uri TempPath = new Uri(info.Directory + "\\" + info.Name);
+                    Uri TempFolder = new Uri(FileDir);
+                    string RelativePath = Uri.UnescapeDataString(TempFolder.MakeRelativeUri(TempPath).ToString().Replace('/', Path.DirectorySeparatorChar));
+                    FileInfoz.Add(new Arquivos(){
+                        FileID = FileID,
+                        FilePath = RelativePath,
+                        FileHash = hashFinal
+                    });
+                    Console.WriteLine("FileID: " + FileID + " - FilePath: " + RelativePath + " - Hash: " + hashFinal);
+                    FileID++;
+                }
+            }
+        }
+        public void FileListWrite() {
+            var json = JsonConvert.SerializeObject(FileInfoz, Formatting.Indented);
+            File.Create("files.json").Dispose();
+            using(var tw = new StreamWriter("files.json")){
+                tw.Write(json);
+            }
+            Console.WriteLine("File List was written on file!");
+        }
+    }
+}
